673c8812002cfa488788fbe50787eb1486298b24
AmbrizAlberto <albertpoambez@gmail.com>
2024-06-30 00:02:06 -0600

    checkbox styles updated

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,7 +5,7 @@ export default function RootLayout({ children }) {
       <head>
         <title>Estetica Matiz</title>
         <meta name="description" content="Agenda tu cita" />
-        <link rel="icon" href="https://cdn-icons-png.flaticon.com/512/72/72179.png" />
+        <link rel="icon" href="https://cdn-icons-png.flaticon.com/512/40/40861.png" />
       </head>
       <body>
         {children}
