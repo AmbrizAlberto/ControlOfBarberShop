ad44595213b15ae88e999b652d01b5bf132ffb20
AmbrizAlberto <albertpoambez@gmail.com>
2024-07-09 23:00:17 -0600

    API Update

--- src/app/api/citas/[id].js
+++ src/app/api/citas/[id].js
@@ -1,10 +1,11 @@
+// src/app/api/citas/[id].js
+
 import { NextResponse } from 'next/server';
 import prisma from '../../../libs/db';
 
 export async function DELETE(request) {
   try {
-    const { searchParams } = new URL(request.url);
-    const id = searchParams.get('id');
+    const { id } = request.params; // Obtén el id del parámetro de la URL
 
     if (!id) {
       return NextResponse.json({ error: 'ID no proporcionado' }, { status: 400 });
