6e347f9e5df9e47200baa032881014df0e87e1fd
AmbrizAlberto <albertpoambez@gmail.com>
2024-07-09 21:31:23 -0600

    Dashboard Styles Update

--- src/app/Dashboard/page.jsx
+++ src/app/Dashboard/page.jsx
@@ -1,18 +1,26 @@
 "use client"
 
-import React from 'react'
-import "../../../public/css/dashb.css"
+import React, { useEffect, useState } from 'react';
+import "../../../public/css/dashb.css";
 
 function Dashboard() {
+  const [citas, setCitas] = useState([]);
+
   const fetchCitas = async () => {
     try {
       const res = await fetch('/api/citas');
-      const citas = await res.json();
+      const citasData = await res.json();
+      setCitas(citasData);
     } catch (error) {
       console.error('Error buscando citas:', error);
     }
   };
-return (
+
+  useEffect(() => {
+    fetchCitas();
+  }, []);
+
+  return (
     <div className='main'>
       <div className='Left'>
         <div className='NewsCreat'>
@@ -22,9 +30,32 @@ return (
       <div className='Right'>
         <div className='ListContainer'>
           <h1>Lista de citas</h1>
+          <table className='citasTable'>
+            <thead>
+              <tr>
+                <th>Nombre</th>
+                <th>Horario de la cita</th>
+                <th>Servicio</th>
+                <th>Tiempo de Servicio</th>
+                <th>Mensaje</th>
+              </tr>
+            </thead>
+            <tbody>
+              {citas.map((cita) => (
+                <tr key={cita.id}>
+                  <td>{cita.clientName}</td>
+                  <td>{new Date(cita.date).toLocaleString()}</td>
+                  <td>{cita.services.join(', ')}</td>
+                  <td>{cita.duration} minutos</td>
+                  <td>{cita.message}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
   );
 }
+
 export default Dashboard;
