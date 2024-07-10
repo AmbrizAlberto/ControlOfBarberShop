124f545d3291eb94c789f285932fa571004ea366
AmbrizAlberto <albertpoambez@gmail.com>
2024-07-09 20:47:45 -0600

    Dashboard

---
+++ src/app/Dashboard/page.jsx
@@ -0,0 +1,12 @@
+"use client"
+
+import React from 'react'
+
+function Dashboard() {
+return (
+    <div className='main'>
+        <div>Dashboard</div>
+    </div>
+  );
+}
+export default Dashboard;
