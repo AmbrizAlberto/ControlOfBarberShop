ecc7757c5611bdad1cac2cec6d48f01667e385ed
AmbrizAlberto <albertpoambez@gmail.com>
2024-07-09 21:42:47 -0600

    API Update

--- src/app/api/citas/route.js
+++ src/app/api/citas/route.js
@@ -139,3 +139,16 @@ export async function POST(request) {
     return NextResponse.json({ error: 'Error al crear la cita' }, { status: 500 });
   }
 }
+
+export async function DELETE(request) {
+  try {
+    const id = request.url.split('/').pop();
+    await prisma.cita.delete({
+      where: { id: parseInt(id) },
+    });
+    return NextResponse.json({ message: 'Cita eliminada exitosamente' });
+  } catch (error) {
+    console.error('Error eliminando la cita:', error);
+    return NextResponse.json({ error: 'Error interno del Servidor' }, { status: 500 });
+  }
+}
