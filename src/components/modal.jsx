14efb77c721d691a1712ab4f5b38693c3234bbbc
AmbrizAlberto <albertpoambez@gmail.com>
2024-07-09 21:49:45 -0600

    Modal Components

---
+++ src/components/modal.jsx
@@ -0,0 +1,23 @@
+// src/components/Modal.js
+
+import React from 'react';
+import '../../public/css/modal.css';
+
+const Modal = ({ show, onClose, onConfirm }) => {
+  if (!show) {
+    return null;
+  }
+
+  return (
+    <div className='modal'>
+      <div className='modal-content'>
+        <h2>Confirmar eliminación</h2>
+        <p>¿Estás seguro de que deseas eliminar esta cita?</p>
+        <button onClick={onConfirm}>Sí</button>
+        <button onClick={onClose}>No</button>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
