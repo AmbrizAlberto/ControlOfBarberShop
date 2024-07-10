63860ddd70b602693c4bead77d84441b21e49774
AmbrizAlberto <albertpoambez@gmail.com>
2024-07-09 21:13:01 -0600

    Dashboard Styles Update

--- src/app/Dashboard/page.jsx
+++ src/app/Dashboard/page.jsx
@@ -1,6 +1,7 @@
 "use client"
 
 import React from 'react'
+import "../../../public/css/dashb.css"
 
 function Dashboard() {
   const fetchCitas = async () => {
@@ -15,12 +16,12 @@ return (
     <div className='main'>
       <div className='Left'>
         <div className='NewsCreat'>
-          Subir noticia
+          <h1>Subir noticia</h1>
         </div>
       </div>
       <div className='Right'>
         <div className='ListContainer'>
-          
+          <h1>Lista de citas</h1>
         </div>
       </div>
     </div>
