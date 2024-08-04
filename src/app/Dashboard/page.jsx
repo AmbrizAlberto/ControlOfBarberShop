82e173a06832a65aa04b02c147de09605c970edc
AmbrizAlberto <albertpoambez@gmail.com>
2024-08-03 17:24:08 -0700

    feat: titles for inputs in News Create

--- src/app/Dashboard/page.jsx
+++ src/app/Dashboard/page.jsx
@@ -162,11 +162,14 @@ function Dashboard() {
             onChange={(e) => setDescription(e.target.value)}
             placeholder="Descripción de la Noticia"
           />
+          Fecha de inicio de Noticia:
           <input
             type="date"
             value={startDate}
             onChange={(e) => setStartDate(e.target.value)}
           />
+          <p></p>
+          Fecha de cierre de Noticia:
           <input
             type="date"
             value={endDate}
